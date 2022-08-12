7e685861dc591957db36737f0a16c48e16c5b97a
Bruk Lemma <bruklemma2017@gmail.com>
2022-08-12 16:07:29 +0300

    file linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,12 +13,12 @@ size_t print_list(const list_t *h)
 	elemt = 0;
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		elemt++;
+			if (h->str == NULL)
+				printf("[%d] %s\n", 0, "(nil)");
+			else
+				printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			elemt++;
 	}
 	return (elemt);
 }
